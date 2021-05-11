a8e2c00f555669ba2842ac67b215199b3f964442
Shambhavi Sharma <60618802+ShambhaviSharma0110@users.noreply.github.com>
2021-05-11 22:15:58 +0530

    Update SignUp.js

--- form/src/Components/SignUp.js
+++ form/src/Components/SignUp.js
@@ -1,9 +1,27 @@
 import React from 'react'
 import {Formik, Form} from 'formik';
 import TextField from "./Components/Textfield";
-
+import * as Yup from 'yup';
 
 const SignUp = () => {
+    const validate = Yup.object({
+      firstName: Yup.string()
+        .max(15, "Must be 15 characters or less")
+        .required("Required"),
+      lastName: Yup.string()
+        .max(20, "Must be 20 characters or less")
+        .required("Required"),
+      email: Yup.string()
+        .email("Email is invalid")
+        .required("Email is required"),
+      password: Yup.string()
+        .min(6, "Password must be at least 6 charaters")
+        .required("Password is required"),
+      confirmPassword: Yup.string()
+        .oneOf([Yup.ref("password"), null], "Password must match")
+        .required("Confirm password is required"),
+    });
+
     return (
       <Formik
       initialValues= {{
